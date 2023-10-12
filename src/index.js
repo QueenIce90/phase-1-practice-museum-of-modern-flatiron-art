051587538239c711ed2eec1c5768c0ce99c6db56
QueenIce90 <Isamargonzalez90@gmail.com>
2023-10-12 15:47:46 -0400

    mockTest

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-console.log('Write your code here');
+
+fetch('http://localhost:3000/current-exhibits')
+.then(response => response.json())
+.then(exhibits => {
+   // exhibits.forEach(exhibit => exhibitsDetail(exhibit))
+    exhibitsDetail(exhibits[0])
+    console.log(exhibits[0])
+})
+
+function exhibitsDetail(exhibit) {
+    const  exhibitTitle = document.querySelector('#exhibit-title')
+    exhibitTitle.textContent = exhibit.title
+    const exhibitDescription = document.querySelector('#exhibit-description')
+    exhibitDescription.textContent = exhibit.description 
+    const exhibitImage = document.querySelector('#exhibit-image')
+    exhibitImage.src = exhibit.image
+    const commmentSection = document.getElementById('comments-section')
+    exhibit.comments.forEach(comment => {
+    
+        const pElement = document.createElement('p')
+        pElement.textContent = comment
+        commmentSection.appendChild(pElement)
+
+  })
+
+    
+}
+
+//Deliverable Two
+
+const commentForm = document.querySelector('#comment-form')
+commentForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    const comment = commentForm[0].value
+    const pElement = document.createElement('p')
+    pElement.textContent = comment
+    const commmentSection = document.getElementById('comments-section')
+    commmentSection.appendChild(pElement)
+
+    //console.log(commentInputElement.value)
+})
+
+
+
+
+
+
+
+ 
+
+
+ 
+  
